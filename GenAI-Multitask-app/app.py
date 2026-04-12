c930cceac79f2adc4d7d7ab05be2d3fbf9f1a1fc
Vigneshrajanengineer <142238618+Vigneshrajanengineer@users.noreply.github.com>
2026-04-12 23:17:36 +0530

    Update app.py

--- GenAI-Multitask-app/app.py
+++ GenAI-Multitask-app/app.py
@@ -71,7 +71,7 @@ if st.button("🚀 Run Task"):
             elif task == "Text Translation":
                 result = client.text_generation(
                     f"Translate to {target_lang}: {input_text}",
-                    model="google/flan-t5-base",
+                    model="zai-org/GLM-5.1",
                     max_new_tokens=200
                 )
                 output = result
