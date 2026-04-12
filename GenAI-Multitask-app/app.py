2d0e8123cc465e48e7dc44353ad0929e5a53d022
Vigneshrajanengineer <142238618+Vigneshrajanengineer@users.noreply.github.com>
2026-04-12 23:31:36 +0530

    Update app.py

--- GenAI-Multitask-app/app.py
+++ GenAI-Multitask-app/app.py
@@ -79,7 +79,7 @@ if st.button("🚀 Run Task"):
             elif task == "Text Writing":
                 result = client.text_generation(
                     f"Write a detailed article about: {input_text}",
-                    model="zai-org/GLM-5.1",
+                    model="openai-community/gpt2",
                     max_new_tokens=300
                 )
                 output = result
